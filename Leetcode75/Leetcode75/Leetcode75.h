7229b5c4d7d6e05a83378488f003fc0e50a83f04
Sarah <sarahireneclark@gmail.com>
2024-10-21 11:58:29 -0500

    Initial commit of project

---
+++ Leetcode75/Leetcode75/Leetcode75.h
@@ -0,0 +1,18 @@
+//
+//  Leetcode75.h
+//  Leetcode75
+//
+//  Created by Sarah Clark on 10/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Leetcode75.
+FOUNDATION_EXPORT double Leetcode75VersionNumber;
+
+//! Project version string for Leetcode75.
+FOUNDATION_EXPORT const unsigned char Leetcode75VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leetcode75/PublicHeader.h>
+
+
---
+++ Leetcode75/Leetcode75Tests/Leetcode75Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Leetcode75Tests.swift
+//  Leetcode75Tests
+//
+//  Created by Sarah Clark on 10/21/24.
+//
+
+import Testing
+@testable import Leetcode75
+
+struct Leetcode75Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
