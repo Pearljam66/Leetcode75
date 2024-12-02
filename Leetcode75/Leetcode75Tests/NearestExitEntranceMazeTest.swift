f133c86957bf9b0c538522bae6b3d8d4f02a3c5f
Sarah <sarahireneclark@gmail.com>
2024-12-02 13:38:05 -0600

    Fixed misspelling

--- Leetcode75/Leetcode75Tests/NearestExitEntranceMazeTest.swift
+++ Leetcode75/Leetcode75Tests/NearestExitEntranceMazeTest.swift
@@ -47,7 +47,7 @@
 
 import Testing
 
-struct NearestExistEntranceMazeTest {
+struct NearestExitEntranceMazeTest {
 
     class Solution {
         func nearestExit(_ maze: [[Character]], _ entrance: [Int]) -> Int {
